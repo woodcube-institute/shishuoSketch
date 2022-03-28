78a8f5ea0f09accc20e225a0e727b0e843a29c54
yuyishi <yishi-yu@outlook.com>
2022-03-28 17:37:34 +0800

    give some message.

--- content/scissors.py
+++ content/scissors.py
@@ -7,7 +7,8 @@ import sys
 
 # function to transfer chinese digits to aribic digits
 # by dodopy https://segmentfault.com/a/1190000013048884
-# with a bit modification 
+# with a bit modification
+# woodcube want to eat icecream.
 def _trans(s):
     digit = {'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9}
     num = 0
